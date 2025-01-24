797c2e53c6ec74a63ffa2d7d8f00c905f1aaab22
Angelina0306 <167233644+Angelina0306@users.noreply.github.com>
2025-01-24 18:42:09 +0300

    hw2.5

--- skript.js
+++ skript.js
@@ -18,3 +18,39 @@ function playgame() {
     num = prompt("Введите число");
   }
 }
+
+
+function playgame2() {
+  function getRandomOperator() {
+    return Math.random() < 0.5 ? "+" : Math.random() < 0.5 ? "-" : "*" || "/";
+  }
+
+  function getRandomNumbers() {
+    let a = Math.floor(Math.random() * 10) + 1;
+    let b = Math.floor(Math.random() * 10) + 1;
+
+    if (getRandomOperator() == "+") {
+      return [a, b, a + b];
+    } else if (getRandomOperator() == "-") {
+      return [a, b, a - b];
+    } else if (getRandomOperator() == "*") {
+      return [a, b, a * b];
+    } else {
+      return [a, b, Math.round(a / b)];
+    }
+  }
+
+  function checkAnswer(userInput) {
+    const randomTask = getRandomNumbers();
+    console.log(`${randomTask[0]} ${randomOperator} ${randomTask[1]} = `);
+
+    if (userInput == randomTask[2]) {
+      console.log("Правильно!");
+    } else {
+      console.log("Неверно, попробуйте ещё раз.");
+    }
+  }
+
+  checkAnswer(prompt("Введите ответ"));
+
+}
